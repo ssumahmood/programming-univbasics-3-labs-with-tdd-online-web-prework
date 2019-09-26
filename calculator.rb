e56748e308d6d7b8b935f7a71224ca3637d5246b
Syed Salah Uddin Mahmood <ssu.mahmood@gmail.com>
2019-09-26 16:08:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 first_number=0# Add your variables here
-second_number=1
+second_number=0
 sum=first_number + second_number
 sum=first_number - second_number
 sum=first_number * second_number
-sum=first_number / second_number
+sum=first_number / second_number!=0
