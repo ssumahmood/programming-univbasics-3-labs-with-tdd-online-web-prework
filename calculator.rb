d9e40c77f7dfcc04883a3b1a8bdb1c4f2eca68c4
Syed Salah Uddin Mahmood <ssu.mahmood@gmail.com>
2019-09-26 16:18:22 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,5 @@ second_number=0
 sum=first_number + second_number
 sum=first_number - second_number
 sum=first_number * second_number
-sum=first_number / second_number rescue ZeroDivisionError
+sum=first_number / second_number 
+rescue ZeroDivisionError
